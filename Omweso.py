5f537417ef9b7ad0d70d4cb9cc34d165529947d8
Lisa-Mimi <118538152+Lisa-Mimi@users.noreply.github.com>
2022-11-20 19:03:09 +0000

    Update Omweso.py

--- Omweso.py
+++ Omweso.py
@@ -1,4 +1,4 @@
-﻿"""class Moves:
+class Moves:
     def __init__(self):
         self.row_new = None
         self.col_new = None
@@ -6,21 +6,32 @@
         self.col_prev = None
         self.winner = None
 
-    def terminal_state(self, player_1, player_2):
+    def terminal_state(self, board_1, board_2):
+        seeds_num = 0  # number of seeds in board 1
+        seeds_num_1 = 0  # number of seeds in board_2
+        for i in range(1):  # number of columns
+            for j in range(7):  # number of rows
+                if board_1[i][j] < 2:
+                    seeds_num = seeds_num + 1
+                elif board_2[i][j] < 2:
+                    seeds_num_1 = seeds_num_1 + 1
+        return seeds_num == 16 or seeds_num_1 == 16
+
+    def winner(self, board_1, board_2):
         # change variable names of num and num_1
-        num = 0  # number of seeds in board 1
-        num_1 = 0  # number of seeds in board_2
+        seeds_num = 0  # number of seeds in board 1
+        seeds_num_1 = 0  # number of seeds in board_2
         for i in range(1): # number of columns
             for j in range(7): # number of rows
-                if player_1[i][j] < 2:
-                    num = num + 1
-                elif player_2[i][j] < 2:
-                    num_1 = num_1 + 1
-        if num == 16 and num_1 < 16:
-            self.winner = player_2
+                if board_1[i][j] < 2:
+                    seeds_num = seeds_num + 1
+                elif board_2[i][j] < 2:
+                    seeds_num_1 = seeds_num_1 + 1
+        if seeds_num == 16 and seeds_num_1 < 16:
+            self.winner = "Player 2"
         else:
-            if num_1 == 16 and num < 16:
-                self.winner = player_1
+            if seeds_num_1 == 16 and seeds_num < 16:
+                self.winner = "Player 1"
         return self.winner
 
     def upper_row_sowing(self, row, col, player, num=0):
@@ -169,21 +180,84 @@
                     num_1 = num_1 + 1
         return num == 16 and num_1 < 16 or num_1 == 16 and num < 16
 
+    def print_boards(self, board_1, board_2):
+        print(board_1[1])
+        print(board_1[0])
+        print()
+        print(board_2[0])
+        print(board_2[1])
+        print()
+
+
 class Omweso(Moves):
     def __init__(self):
         super().__init__()
-        self.player_0 = [[4, 4, 4, 4, 4, 4, 4, 4],
+        self.board_1 = [[4, 4, 4, 4, 4, 4, 4, 4],
                     [0, 0, 0, 0, 0, 0, 0, 0]]
-        self. player_1 = [[4, 4, 4, 4, 4, 4, 4, 4],
+        self.board_2 = [[4, 4, 4, 4, 4, 4, 4, 4],
                     [0, 0, 0, 0, 0, 0, 0, 0]]
+        print("________Boards:_______")
+        self.print_boards(self.board_1, self.board_2)
+        self.curr_player_name = None
+        self.curr_board = None
+        self.opp_board = None
 
     def playing(self, player_1_name, player_2_name):
-        curr_player = self.player_1
-        curr_player_name = player_1_name
-        while True:"""
+        self.curr_board = self.board_1
+        self.opp_board = self.board_2
+        self.curr_player_name = player_1_name
+
+        while True:
+            print(self.curr_player_name + " is playing")
+            row, col = input("Enter row<Enter> and column:").split(" ")
+            row = int(row)
+            col = int(col)
+
+            self.sowing(row, col, self.curr_board)
+            print("Board after sowing")
+            self.print_boards(self.board_1, self.board_2)
+
+            if self.is_capture(self.opp_board, self.curr_board):
+                self.capture(self.opp_board, self.curr_board)
+                print("Board after capture:")
+                self.print_boards(self.board_1, self.board_2)
+
+            elif self.is_reverse_capture(self.opp_board, self.curr_board):
+                self.reverse_capture(self.opp_board, self.curr_board)
+                print("Board after reverse capture:")
+                self.print_boards(self.board_1, self.board_2)
+
+            elif self.is_relay_sowing(self.curr_board):
+                self.relay_sowing(self.curr_board)
+                print("Board after relay sowing")
+                self.print_boards(self.board_1, self.board_2)
+
+            elif self.terminal_state(self.board_1, self.board_2):
+                    winner = self.winner(self.board_1, self.board_2)
+                    print("________Board for terminal state_______")
+                    self.print_boards(self.board_1, self.board_2)
+                    if winner == "Player 1":
+                        print(player_1_name + " has won!")
+                    if winner == "Player 2":
+                        print(player_2_name + " has won!")
+                    return
+
+            else:
+                if self.curr_board == self.board_1:
+                    self.curr_board = self.board_2
+                    self.opp_board = self.board_1
+                    self.curr_player_name = player_2_name
+
+                elif self.curr_board == self.board_2:
+                    self.curr_board = self.board_1
+                    self.opp_board = self.board_2
+                    self.curr_player_name = player_1_name
 
 
-
+obj = Omweso()
+player_1_name = input("Enter the name of the player 1:")
+player_2_name = input("Enter the name of the player 2:")
+obj.playing(player_1_name, player_2_name)
 
 
 
